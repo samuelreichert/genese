66f7153423eedce4cb9f7f06f5b0d3ce028ebe98
Samuel Reichert <samuelreichertt@gmail.com>
2015-12-08 19:39:44 -0200

    Update dashboard and contact_form controllers

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -1,11 +1,11 @@
 class ContactFormsController < ApplicationController
-  before_action :set_contact_form, only: [:show, :edit, :update, :destroy]
+  # before_action :set_contact_form, only: [:show, :edit, :update, :destroy]
 
   # GET /contact_forms
   # GET /contact_forms.json
-  def index
-    @contact_forms = ContactForm.all
-  end
+  # def index
+  #   @contact_forms = ContactForm.all
+  # end
 
   # GET /contact_forms/1
   # GET /contact_forms/1.json
@@ -18,8 +18,8 @@ class ContactFormsController < ApplicationController
   end
 
   # GET /contact_forms/1/edit
-  def edit
-  end
+  # def edit
+  # end
 
   # POST /contact_forms
   # POST /contact_forms.json
@@ -39,33 +39,33 @@ class ContactFormsController < ApplicationController
 
   # PATCH/PUT /contact_forms/1
   # PATCH/PUT /contact_forms/1.json
-  def update
-    respond_to do |format|
-      if @contact_form.update(contact_form_params)
-        format.html { redirect_to @contact_form, notice: 'Contact form was successfully updated.' }
-        format.json { render :show, status: :ok, location: @contact_form }
-      else
-        format.html { render :edit }
-        format.json { render json: @contact_form.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @contact_form.update(contact_form_params)
+  #       format.html { redirect_to @contact_form, notice: 'Contact form was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @contact_form }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @contact_form.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /contact_forms/1
   # DELETE /contact_forms/1.json
-  def destroy
-    @contact_form.destroy
-    respond_to do |format|
-      format.html { redirect_to contact_forms_url, notice: 'Contact form was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @contact_form.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to contact_forms_url, notice: 'Contact form was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_contact_form
-      @contact_form = ContactForm.find(params[:id])
-    end
+    # def set_contact_form
+    #   @contact_form = ContactForm.find(params[:id])
+    # end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def contact_form_params
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,14 +1,8 @@
 class DashboardController < ApplicationController
-  before_filter :set_locale
-
   def index
     # @account = current_account
 
     # @balance = @account.total_balance
     # @expenditure = @account.total_expenditure
-  end
-
-  def set_locale
-    I18n.locale = http_accept_language.compatible_language_from(I18n.available_locales)
   end
 end
