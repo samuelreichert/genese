2b9a294c4b2bcfeba05e347523b11c8eef406105
Samuel Reichert <samuelreichertt@gmail.com>
2015-12-15 21:43:42 -0200

    Implement paperclip + aws S3 to image upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,13 @@ class User < ActiveRecord::Base
 
   has_and_belongs_to_many :accounts
 
-  mount_uploader :picture, PictureUploader
+  # This method associates the attribute ":avatar" with a file attachment
+  has_attached_file :avatar, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
