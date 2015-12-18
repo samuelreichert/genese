7e4c16f4def668ea7eb53bd3f7760eea2b63ac89
Samuel Reichert <samuelreichertt@gmail.com>
2015-12-18 20:35:08 -0200

    Add formatted date helper

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,2 +1,6 @@
 module EntriesHelper
+  def formatted_current_date(current_date)
+    date = "&nbsp; #{I18n.t("date.month_names")[current_date.month]} de #{current_date.year} &nbsp;"
+    date.html_safe
+  end
 end
