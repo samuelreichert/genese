cd5ebeba9aa3e45f6d1d5c7a24985edd27681a33
Samuel Reichert <samuelreichertt@gmail.com>
2015-12-19 00:48:19 -0200

    Update user permitted param to avatar

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,3 +1,3 @@
 ActiveAdmin.register User do
-  permit_params :email, :encrypted_password, :name, :picture, :birth_date, :main_account
+  permit_params :email, :encrypted_password, :name, :avatar, :birth_date, :main_account
 end
