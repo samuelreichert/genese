d994cc98e0c501f739dfb2456272f1391a13ca4b
Samuel Reichert <samuelreichertt@gmail.com>
2016-01-08 21:54:23 -0200

    Update seeds to project

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 
+# Create Default Categories
 default_categories_list = [
   ['Alimentação', '#f1c40f'],
   ['Educação', '#27ae60'],
@@ -21,3 +22,14 @@ default_categories_list.each do |name, color|
   DefaultCategory.create( name: name, color: color )
 end
 
+# Create test User
+user = User.create!(email: 'teste@teste.com', password: 'teste1234', password_confirmation: 'teste1234', name: 'Teste', birth_date: '01/01/1990')
+
+# Create test Account
+account = user.accounts.create!(reminder_days_before: 3, reminder_active: true, name: "Conta Privada - #{user.name}", owner: user.id)
+user.update(main_account: account.id)
+
+# Create categories for test
+DefaultCategory.find_each do |category|
+  account.categories.create(category.attributes.except('id'))
+end
